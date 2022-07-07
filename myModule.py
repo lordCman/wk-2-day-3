6cc9e73a43db828e66d0fb93a003aede6da18c65
lordCman <crodriguez1086@yahoo.com>
2022-07-07 18:07:56 -0400

    last

---
+++ myModule.py
@@ -0,0 +1,13 @@
+def sqft():
+    x = int(input("what is the length of your house? (#)"))
+    y = int(input("what us the width of your house? (#)"))
+    A = (x * y)
+    print(f"the area of your house is {A} ")
+
+    
+def circum_cir():
+    pi = 3.14
+    r = int(input("what is the radius of your circle? (#)"))
+    C = (2 * pi * r)
+    print(f"the circumference of your circle is {C}")
+
